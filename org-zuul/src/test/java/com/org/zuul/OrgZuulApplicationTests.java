61c45491f8dd517ed386353f18fec837c61fa65a
ganesh999999 <52598986+ganesh999999@users.noreply.github.com>
2019-12-01 11:59:59 +0530

    d
    dsf

---
+++ org-zuul/src/test/java/com/org/zuul/OrgZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
